1323ccf63d12c8c48296614175343494a2a4b918
Mohan <otienomohan678@gmail.com>
2025-07-29 13:52:32 +0300

    Simple calculator in python

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+# Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Ask the user for the operation
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the chosen operation
+if operation == '+':
+    result = num1 + num2
+    print(f"The result of {num1} + {num2} is {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"The result of {num1} - {num2} is {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"The result of {num1} * {num2} is {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result of {num1} / {num2} is {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please enter +, -, *, or /.")
